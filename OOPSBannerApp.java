410baa3332ab7a540d29abdcca69ecafa808158d
iimanbucchelvan-del <iimanbucchelvan@gmail.com>
2026-02-17 09:50:32 +0530

    Add files via upload

---
+++ OOPSBannerApp.java
@@ -0,0 +1,16 @@
+public class OOPSBannerApp{
+
+	public static void main(String[] args) {
+		// Hint: Use the Example below as a guide to construct each line of the banner
+		// Banner for OOPS
+		System.out.println("  *****   " + "  *****   " + "  *****   " + "  *****   ");
+		System.out.println(" **   **  " + " **   **  " + " **   **  " + " **       ");
+		System.out.println("**     ** " + "**     ** " + "**     ** " + "**        ");
+		System.out.println("**     ** " + "**     ** " + "*******   " + "  *****   ");
+		System.out.println("**     ** " + "**     ** " + "**        " + "       ** ");
+		System.out.println(" **   **  " + " **   **  " + "**       " + " **    ** ");
+		System.out.println("  *****   " + "  *****   " + "**       " + "  *****   ");
+
+
+		}
+	}
