668faf68fc529d32f341fbd541003c4e6752919d
iimanbucchelvan-del <iimanbucchelvan@gmail.com>
2026-02-24 09:23:23 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,6 +1,17 @@
 public class OOPSBannerApp{
 
 	public static void main(String[] args) {
-		System.out.println(x:"OOPS");
+		lines[0] = String.join("   ", " *** ", " *** ", " **** ", " **** ");
+        lines[1] = String.join("   ", "*   *", "*   *", "*   *", "*   *");
+        lines[2] = String.join("   ", "*   *", "*   *", "*   *", "*    ");
+        lines[3] = String.join("   ", "*   *", "*   *", " ****", " ****");
+        lines[4] = String.join("   ", "*   *", "*   *", "*    ", "    *");
+        lines[5] = String.join("   ", "*   *", "*   *", "*    ", "*   *");
+        lines[6] = String.join("   ", " *** ", " *** ", "*    ", " ****");
+		
+		for(String line:lines){
+			System.out.println(line);
 		}
+		}
+
 	}
